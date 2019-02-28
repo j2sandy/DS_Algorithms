f966d2c703cea7b7a2d5962977aa4a02da4926dd
nitin <nitinbinay734@gmail.com>
2019-03-01 00:54:57 +0530

    first commit in nishandi

---
+++ src/io/nishandi/ds/tree/BinaryTree.java
@@ -0,0 +1,21 @@
+package io.nishandi.ds.tree;
+
+public class BinaryTree {
+
+	static class Node
+	{
+		Node left,right;
+		int val;
+		public Node(int val)
+		{
+			this.val=val;
+			this.left=this.right=null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
